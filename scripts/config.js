b4f736a6fa4a527a64bf37139ee3f078a6bc4fff
Paul.U78 <150560981+NewGate78@users.noreply.github.com>
2024-01-08 11:00:00 +0100

    Add files via upload
    mini projet name azertypa

---
+++ scripts/config.js
@@ -0,0 +1,25 @@
+/*********************************************************************************
+ *
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ *
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = [
+  "Pomme",
+  "Chaise",
+  "Voiture",
+  "Table",
+  "Ordinateur",
+  "Maison",
+  "Chat",
+  "Arbre",
+];
+const listePhrases = [
+  "La vie est belle.",
+  "La curiosité est un vilain défaut.",
+  "Le temps passe, les souvenirs restent.",
+  "L'amour donne des ailes.",
+  "Le silence est d'or.",
+];
